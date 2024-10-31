3005ecb565f70b7d99f6b0174a0644c8fb501902
Saikat Sarkar <saikatsarkar10@outlook.com>
2024-10-31 23:29:20 +0530

    added auto refresh

--- src/pages/Borad/BoardPage.js
+++ src/pages/Borad/BoardPage.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './BoardPage.css'
 import { BsPeople } from "react-icons/bs";
 import { SlArrowDown } from "react-icons/sl";
@@ -14,12 +14,12 @@ import { ToastContainer, toast } from "react-toastify";
 function getDate() {
   const today = new Date();
   const day = today.getDate();
-  const month = today.toLocaleString('default', { month: 'short' });
+  const month = today.toLocaleString( 'default', { month: 'short' } );
   const year = today.getFullYear();
 
-  const getDaySuffix = (day) => {
-    if (day > 3 && day < 21) return 'th';
-    switch (day % 10) {
+  const getDaySuffix = ( day ) => {
+    if ( day > 3 && day < 21 ) return 'th';
+    switch ( day % 10 ) {
       case 1: return 'st';
       case 2: return 'nd';
       case 3: return 'rd';
@@ -27,82 +27,82 @@ function getDate() {
     }
   };
 
-  return `${day}${getDaySuffix(day)} ${month}, ${year}`;
+  return `${ day }${ getDaySuffix( day ) } ${ month }, ${ year }`;
 }
 
 
 
 const BoardPage = () => {
-  const [currentDate, setCurrentDate] = useState(getDate());
-  const [isTaskModalOpen,setIsTaskModalOpen]=useState(false)
-  const [isAddPeopleModalOpen,setIsAddPeopleModalOpen]=useState(false)
-  const [tasks, setTasks] = useState({ todo: [], backlog: [], inProgress: [], done: [] });
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [filterValue, setFilterValue] = useState("WEEK"); 
+  const [currentDate, setCurrentDate] = useState( getDate() );
+  const [isTaskModalOpen, setIsTaskModalOpen] = useState( false )
+  const [isAddPeopleModalOpen, setIsAddPeopleModalOpen] = useState( false )
+  const [tasks, setTasks] = useState( { todo: [], backlog: [], inProgress: [], done: [] } );
+  const [loading, setLoading] = useState( true );
+  const [error, setError] = useState( null );
+  const [filterValue, setFilterValue] = useState( "WEEK" );
 
 
-  const fetchTasks = async (filter) => {
-    setLoading(true);
+  const fetchTasks = async ( filter ) => {
+    setLoading( true );
     try {
-      const response = await getTasks(filter); // Pass the filter to your API call
-      if (response?.message === "Data Fetched Successfully") {
-        const fetchedTasks = response.task; 
-        console.log(fetchedTasks)
+      const response = await getTasks( filter ); // Pass the filter to your API call
+      if ( response?.message === "Data Fetched Successfully" ) {
+        const fetchedTasks = response.task;
+        console.log( fetchedTasks )
         const categorizedTasks = {
           todo: fetchedTasks.todo || [],
           backlog: fetchedTasks.backLog || [],
           inProgress: fetchedTasks.inProgress || [],
           completed: fetchedTasks.done || []
         };
-        setTasks(categorizedTasks);
-        console.log(categorizedTasks)
+        setTasks( categorizedTasks );
+        console.log( categorizedTasks )
       } else {
-        setError(response);
+        setError( response );
       }
-    } catch (err) {
-      setError(err);
+    } catch ( err ) {
+      setError( err );
     } finally {
-      setLoading(false);
+      setLoading( false );
     }
   };
 
-  useEffect(() => {
-    fetchTasks(filterValue);
-  }, [filterValue]); 
+  useEffect( () => {
+    fetchTasks( filterValue );
+  }, [filterValue] );
 
 
-  const isOpen=()=>{
-    setIsTaskModalOpen(true)
+  const isOpen = () => {
+    setIsTaskModalOpen( true )
   }
-  const onClose=()=>{
-    setIsTaskModalOpen(false)
+  const onClose = () => {
+    setIsTaskModalOpen( false )
   }
-  const isAddPeopleOpen=()=>{
-    setIsAddPeopleModalOpen(true)
+  const isAddPeopleOpen = () => {
+    setIsAddPeopleModalOpen( true )
   }
-  const onAddPeopleClose=()=>{
-    setIsAddPeopleModalOpen(false)
+  const onAddPeopleClose = () => {
+    setIsAddPeopleModalOpen( false )
   }
-  const handleFilterChange = (e) => {
-    setFilterValue(e.target.value);
+  const handleFilterChange = ( e ) => {
+    setFilterValue( e.target.value );
   };
 
-  if (loading) {
+  if ( loading ) {
     return (
       <div className="loading-overlay">
-    <div className="loading-container">
-      <p>Loading tasks...</p>
-      <div className="spinner"></div>
-    </div>
-  </div>
+        <div className="loading-container">
+          <p>Loading tasks...</p>
+          <div className="spinner"></div>
+        </div>
+      </div>
     );
   }
 
-  if (error) {
+  if ( error ) {
     return <p>Error loading tasks: {error.message || 'Something went wrong'}</p>;
   }
-  
+
 
   return (
     <div>
@@ -112,88 +112,88 @@ const BoardPage = () => {
           <h3>Welcome! Soumen</h3>
         </div>
         <div className="">
-        <p>{currentDate}</p>
+          <p>{currentDate}</p>
         </div>
       </div>
       <div className="section2">
-      <div className="sub-section">
-        <div className=""><h2 className="">Board</h2></div>
-        <div className="">
-        <button className="button1" onClick={isAddPeopleOpen}>
-        <BsPeople />
-          <p className="btn-text">Add People</p>
-        </button>
+        <div className="sub-section">
+          <div className=""><h2 className="">Board</h2></div>
+          <div className="">
+            <button className="button1" onClick={isAddPeopleOpen}>
+              <BsPeople />
+              <p className="btn-text">Add People</p>
+            </button>
+          </div>
+
         </div>
-       
-      </div>
-      <div className="sub-section1">
-        
-       
-        <label className="dropdown">
+        <div className="sub-section1">
 
-  <div className="dd-button">
-  {filterValue}
-  </div>
 
-  <input type="checkbox" className="dd-input" id="test" />
+          <label className="dropdown">
 
-  <ul className="dd-menu">
-    <li onClick={() => handleFilterChange({ target: { value: "TODAY" } })}>Today</li>
-    <li onClick={() => handleFilterChange({ target: { value: "WEEK" } })}>This Week</li>
-    <li onClick={() => handleFilterChange({ target: { value: "MONTH" } })}>This Month</li>
-    
-   
-  </ul>
-  
-</label>
+            <div className="dd-button">
+              {filterValue}
+            </div>
+
+            <input type="checkbox" className="dd-input" id="test" />
+
+            <ul className="dd-menu">
+              <li onClick={() => handleFilterChange( { target: { value: "TODAY" } } )}>Today</li>
+              <li onClick={() => handleFilterChange( { target: { value: "WEEK" } } )}>This Week</li>
+              <li onClick={() => handleFilterChange( { target: { value: "MONTH" } } )}>This Month</li>
+
+
+            </ul>
+
+          </label>
+        </div>
       </div>
-      </div>
-     
+
       <div className="section3">
         <div className="section-box">
           <div className="hedaer-box">
             <div className=""><h4 className="">Backlog</h4></div>
             <button type="" className="button-header"><TfiLayers /></button>
           </div>
-          {tasks?.backlog?.map((task, index) => (
-            <TaskBox key={task.id} task={task} />
-          ))}
-          
+          {tasks?.backlog?.map( ( task, index ) => (
+            <TaskBox key={task.id} task={task} filterValue={filterValue} fetchTasks={fetchTasks} />
+          ) )}
+
 
         </div>
         <div className="section-box">
-        <div className="hedaer-box">
+          <div className="hedaer-box">
             <div className=""><h4 className="">To Do</h4></div>
             <div className="">
               <button type="" className="button-header" onClick={isOpen}><FaPlus /></button>
-            <button type="" className="button-header"><TfiLayers /></button>
-          
+              <button type="" className="button-header"><TfiLayers /></button>
+
+            </div>
           </div>
-          </div>
-          {tasks.todo.map((task) => (
-            <TaskBox key={task.id} task={task} />
-          ))}
+          {tasks.todo.map( ( task ) => (
+            <TaskBox key={task.id} task={task} filterValue={filterValue} fetchTasks={fetchTasks} />
+          ) )}
         </div>
         <div className="section-box">
-        <div className="hedaer-box">
+          <div className="hedaer-box">
             <div className=""><h4 className="">In Progrss</h4></div>
             <button type="" className="button-header"><TfiLayers /></button>
           </div>
         </div>
         <div className="section-box">
-        <div className="hedaer-box">
+          <div className="hedaer-box">
             <div className=""><h4 className="">Completed</h4></div>
             <div className="">
-            <button type="" className="button-header"><TfiLayers /></button>
+              <button type="" className="button-header"><TfiLayers /></button>
+            </div>
           </div>
-          </div>
-          {tasks.completed.map((task) => (
-            <TaskBox key={task.id} task={task} />
-          ))}
+          {tasks.completed.map( ( task ) => (
+            <TaskBox key={task.id} task={task} filterValue={filterValue} fetchTasks={fetchTasks} />
+          ) )}
         </div>
       </div>
       {
-        isTaskModalOpen && <TaskModal onClose={onClose} />
+        isTaskModalOpen && <TaskModal onClose={onClose} filterValue={filterValue} fetchTasks={fetchTasks} />
       }
       {
         isAddPeopleModalOpen && <AddPeopleModal onAddPeopleClose={onAddPeopleClose} />
--- src/pages/ModalPage/DeleteModal.js
+++ src/pages/ModalPage/DeleteModal.js
@@ -4,26 +4,27 @@ import { RxCross2 } from "react-icons/rx";
 import { deleteTask } from '../../api/apiClient';
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-const DeleteModal = ({  onCloseDeleteModal,taskID }) => {
+const DeleteModal = ( { onCloseDeleteModal, taskID } ) => {
 
   // Function to handle the form submission (POST method)
-  const handleSubmit = async (e) => {
+  const handleSubmit = async ( e ) => {
     e.preventDefault();
 
     try {
-      const result = await deleteTask(taskID);
-      
-      if (result.message === 'Task and associated checklist deleted successfully') {
-        console.log(result.message);
-        toast.success("task deleted Success fully")
+      const result = await deleteTask( taskID );
+
+      if ( result.message === 'Task and associated checklist deleted successfully' ) {
+        console.log( result.message );
+        toast.success( "task deleted Success fully" )
+        // fetchTasks( filterValue )
         onCloseDeleteModal()
 
 
       } else {
-        console.error('Error:', result.message); 
+        console.error( 'Error:', result.message );
       }
-    } catch (error) {
-      console.error('Error while deleting task:', error);
+    } catch ( error ) {
+      console.error( 'Error while deleting task:', error );
     }
   };
 
@@ -35,13 +36,13 @@ const DeleteModal = ({  onCloseDeleteModal,taskID }) => {
       <div className="card">
         <div className="card-content">
           <p className="card-heading">Are you sure want to Delete?</p>
-         
+
         </div>
         <div className="card-button-wrapper">
           <button className="card-button-cancel1 secondary" onClick={onCloseDeleteModal}>Cancel</button>
           <button className="card-button-delete primary" onClick={handleSubmit}>Yes, Delete</button>
         </div>
-      
+
       </div>
     </div>
   );
